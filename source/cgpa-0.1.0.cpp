eef9207e602405bd800da9be2e9323a9322ffb2b
Injamamul Haque Sourov <42240780+injamam-sourov@users.noreply.github.com>
2019-08-29 13:11:46 +0600

    Update cgpa-0.1.0.cpp

--- source/cgpa-0.1.0.cpp
+++ source/cgpa-0.1.0.cpp
@@ -1,22 +1,25 @@
-// CGPA calculator v0.1 - refactored code, added waiver feature
+// CGPA calculator v0.1.0 - refactored code, added waiver feature
 /*
 Changelog: 
-[1] [important] tuition waiver based on cgpa feature added
-[2] calculation of course grade points merged with course details input
-[3] total credit and total grade point earned merged with course details input
+[1] [important] tuition waiver based on cgpa feature added as a function
+[2] [important] course id limited to 7 characters to improve visual corrections for output table
+[3] array courseName changed to courseId 
+[4] calculation of course grade points merged with course details input
+[5] total credit and total grade point earned merged with course details input
+[6] opening message stored in a constant MESSAGE
+[7] course table and cgpa output made a seperate function
 
 */
 #include <iostream>
 using namespace std;
+const string MESSAGE = "# To start, enter the number of courses you wish to evaluate.\n# Course id is optional and can be left blank. However, there's a maximum limit of 7 characters (eg. CSE 123)\n# Course credit can be 0,1 or 3 only\n# Course grade is case insensitive ('B+' is same as 'b+') and can be any value from A though D and F only\n# WARNING: Care should be taken as not all inputs are extensively validated\n";
+void waiver(float cgpa);
+void output(int numberOfCourses, string courseId[], int courseCredit[], string courseGrade[], float courseGradePoint[], float cgpa);
 
 int main() {
     cout << "------- Generic GPA Calculator -------\n" << endl;
-    cout << "# To start, enter the number of courses you wish to evaluate.\n# Course credit can be 0,1 or 3 only\n" <<
-            "# Course grade is case insensitive ('B+' is same as 'b+') and can be any value from A though D and F only\n" <<
-            "# Course name/id is optional but for properp alignment of output table, use consistent naming conventions " << 
-            "(eg. CSC121, EEE221 OR CSC 121, eee 221 OR leave ALL blank)\n" << 
-            "# WARNING: Care should be taken as not all inputs are extensively validated\n" << endl;
-
+	cout << MESSAGE << endl;
+	
     // get number of courses taken this semester [add validations]
     int numberOfCourses;
     cout << "Number of courses completed? ";
@@ -31,18 +34,24 @@ int main() {
     }
 
     // get course names, credits and grades [add validations to each]
-    string courseName[numberOfCourses], courseGrade[numberOfCourses];
+    string courseId[numberOfCourses], courseGrade[numberOfCourses];
     int courseCredit[numberOfCourses], totalCredit=0; 
     float courseGradePoint[numberOfCourses], totalGradePoint=0, cgpa=0;
     for(int i=0; i<numberOfCourses; i++) { // [2] 
-        cout << "("<< i+1 << ") Course " << " name (Optional): ";
-        getline(cin, courseName[i]);
+        cout << "("<< i+1 << ") Course " << " Id (Optional): ";
+        getline(cin, courseId[i]);
+            // validations on course name
+            if(courseId[i].length()>7){
+                cout << "course id should not exceed 7 charecters. Try EEE123 or EEE 123 or leave blank, terminating program!" << endl;
+                system("pause"); // [3]
+                return 0;
+            }
         cout << "Credit(s): ";
         cin >> courseCredit[i];
         cin.get();  // [1]
-            //validation on course credit
-            if(courseCredit[i] !=3 && courseCredit[i] !=1 && courseCredit[i] !=0){
-                cout << "a course can be of 0, 1 or 3 credits, terminating program!" << endl;
+            //validation on course credit [4]
+            if(courseCredit[i] !=4 && courseCredit[i] !=3 && courseCredit[i] !=1 && courseCredit[i] !=0){
+                cout << "a course can be of 0, 1, 3 or 4 credits, terminating program!" << endl;
                 system("pause"); // [3]
                 return 0;
             }
@@ -85,17 +94,16 @@ int main() {
     cgpa = totalGradePoint/totalCredit;
 
     // output course table and cgpa
-    string space;
-    cout << "\nName - Credit - Grade - Point" << endl;
-    for(int i=0; i<numberOfCourses; i++){
-        //visual corrections for consistent naming
-        if(courseGrade[i].length() == 2) space="      "; else space="       ";
-        if(courseName[i] == "") courseName[i]= "    ";
-        cout << courseName[i] << "   " << courseCredit[i] << "        " << courseGrade[i] << space << courseGradePoint[i] << endl;
-    } 
-    cout << "\nCGPA: " << cgpa << endl;;
-    
-    //IUB waiver calculations as of autumn 2019
+    output(numberOfCourses, courseId, courseCredit, courseGrade, courseGradePoint, cgpa);
+
+    //calculate waiver
+    waiver(cgpa);
+
+    system("pause"); // [3]
+    return 0;
+}
+//IUB waiver calculations as of autumn 2019
+void waiver(float cgpa){
     string waiver; 
     if(cgpa>=3.845) waiver="100%";
     else if(cgpa>=3.795 && cgpa<=3.844) waiver="75%";
@@ -104,9 +112,47 @@ int main() {
     else if(cgpa>=3.255 && cgpa<=3.504) waiver="20% (only for students enrolled before summer 2019)";
     else waiver="none";
     cout << "Tuition waiver: " << waiver << endl;
+}
 
-    system("pause"); // [3]
-    return 0;
+// generate a course table and print cgpa
+void output(int numberOfCourses, string courseId[], int courseCredit[], string courseGrade[], float courseGradePoint[], float cgpa){
+    string space;
+    cout << "\nCourseId - Credit - Grade - Point" << endl;
+    for(int i=0; i<numberOfCourses; i++){
+        //visual corrections for output table
+        if(courseGrade[i].length() == 2) space="      "; else space="       ";
+        switch (courseId[i].length())
+        {
+        case 0:
+            courseId[i].append("        ");
+            break;
+        case 1:
+            courseId[i].append("       ");
+            break;
+        case 2:
+            courseId[i].append("      ");
+            break;
+        case 3:
+            courseId[i].append("     ");
+            break;
+        case 4:
+            courseId[i].append("    ");
+            break;
+        case 5:
+            courseId[i].append("   ");
+            break;
+        case 6:
+            courseId[i].append("  ");
+            break;
+        case 7:
+            courseId[i].append(" ");
+            break;
+        default:
+            break;
+        }
+        cout << courseId[i] << "   " << courseCredit[i] << "        " << courseGrade[i] << space << courseGradePoint[i] << endl;
+    } 
+    cout << "\nCGPA: " << cgpa << endl;
 }
 
 /* remarks:
@@ -114,6 +160,7 @@ int main() {
 getline making it seem as if the first getline input within the loop is skipped.
 [2] if loop condition changed to i=1; i<=numberOfCourses; i++ it doesn't work. why?
 [3] temporary soltion to prevent .exe from exiting immediately.
+[4][PROBLEM] NaN input for credit does not give the required error message
 */
 
 /* program structure:
